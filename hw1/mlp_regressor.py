f1eaea7269b7254038fa21e83bdbadf0cf88361e
mootheerloodee <tasya15.2000@gmail.com>
2025-09-10 16:31:39 +0300

    feat(hw1): Добавлена реализация MLP регрессора

--- hw1/mlp_regressor.py
+++ hw1/mlp_regressor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MLPRegressor:
     """
     Многослойный перцептрон (MLP) для задачи регрессии, использующий алгоритм
@@ -17,6 +19,18 @@ class MLPRegressor:
         """
         self.hidden_layer_sizes = hidden_layer_sizes
         self.learning_rate = learning_rate
+        self.n_iter = n_iter
+        self.coef_ = None
+        self.intercept_ = None
+        self.a_ = None
+
+    def sigmoid(self, z):
+        z = np.clip(z, -500, 500)
+        return 1 / (1 + np.exp(-z))
+
+    def sigmoid_derivative(self, a):
+        return a * (1 - a)
+
 
     def forward(self, X):
         """
@@ -28,7 +42,16 @@ class MLPRegressor:
         Возвращает:
             np.ndarray: Предсказания модели
         """
-        pass
+        self.a_ = [X]
+        curr_a = X
+        for i in range(len(self.coef_) - 1):
+            z = np.dot(curr_a, self.coef_[i]) + self.intercept_[i]
+            curr_a = self.sigmoid(z)
+            self.a_.append(curr_a)
+        tmp = np.dot(curr_a, self.coef_[-1]) + self.intercept_[-1]
+        self.a_.append(tmp)
+        return tmp
+
 
     def backward(self, X, y):
         """
@@ -38,7 +61,20 @@ class MLPRegressor:
             X (np.ndarray): Матрица признаков размера (n_samples, n_features)
             y (np.ndarray): Вектор таргета длины n_samples
         """
-        pass
+        n_objects, n_features = X.shape
+        coef_grad = []
+        intercept_grad = []
+        y_predict = self.forward(X)
+        delta = 2 / n_objects * (y_predict - y)
+        for i in range(len(self.coef_) - 1, -1, -1):
+            cur_coef_grad = np.dot(self.a_[i].T, delta)
+            cur_intercept_grad = np.sum(delta)
+            coef_grad.insert(0, cur_coef_grad)
+            intercept_grad.insert(0, cur_intercept_grad)
+            if i > 0:
+                error = np.dot(delta, self.coef_[i].T) * self.sigmoid_derivative(self.a_[i])
+        return coef_grad, intercept_grad
+
 
     def fit(self, X, y):
         """
@@ -48,7 +84,21 @@ class MLPRegressor:
             X (np.ndarray): Матрица признаков размера (n_samples, n_features)
             y (np.ndarray): Вектор таргета длины n_samples
         """
-        pass
+        n_objects, n_features = X.shape
+        layers_sizes = [n_features] + list(self.hidden_layer_sizes) + [1]
+        self.coef_ = []
+        self.intercept_ = []
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+        for i in range(len(layers_sizes) - 1):
+            self.coef_.append(np.random.randn(layers_sizes[i], layers_sizes[i+1]) * 0.1)
+            self.intercept_.append(np.zeros((1, layers_sizes[i + 1])))
+        for i in range(self.n_iter):
+            coef_grad, intercept_grad = self.backward(X, y)
+            for j in range(len(self.coef_)):
+                self.coef_[j] -= self.learning_rate * coef_grad[j]
+                self.intercept_[j] -= self.learning_rate * intercept_grad[j]
+
 
     def predict(self, X):
         """
@@ -60,4 +110,4 @@ class MLPRegressor:
         Возвращает:
             np.ndarray: Предсказание для каждого элемента из X
         """
-        pass
+        return self.forward(X)
